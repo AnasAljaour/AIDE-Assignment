f0f21b4dbf5b976357ddd2261df4dad13c2efb8c
AnasAljaour <anas.aljaour13@gmail.com>
2025-02-23 21:31:15 +0200

    Update Flask Server

--- Flask-API/app.py
+++ Flask-API/app.py
@@ -2,8 +2,6 @@ from flask import Flask, request, jsonify
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 analyzer = SentimentIntensityAnalyzer()
-text = "This movie was terrible. I hated it!"
-sentiment = analyzer.polarity_scores(text)
 
 app = Flask(__name__)
 @app.route('/predict', methods = ['POST'])
@@ -13,13 +11,13 @@ def predict():
         return jsonify({"error": "Invalid input, 'text' is required"}), 400
     text = data['text']
     sentiment = analyzer.polarity_scores(text)
-    match sentiment['compound']:
-        case x if x <= -0.05:
-            return jsonify({"sentiment": "negative"}), 200
-        case x if x >= 0.05:
-            return jsonify({"sentiment": "positive"}), 200
-        case _:
-            return jsonify({"sentiment": "neutral"}), 200
+    x = sentiment['compound']
+    if x <= -0.05:
+        return jsonify({"sentiment": "negative"}), 200
+    if x >= 0.05:
+        return jsonify({"sentiment": "positive"}), 200
+ 
+    return jsonify({"sentiment": "neutral"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
